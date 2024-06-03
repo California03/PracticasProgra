3d3a536a301594128e644adf318fe0561454f574
California03 <156863607+California03@users.noreply.github.com>
2024-06-03 15:06:42 -0600

    Calculadora
    Primera parte de la calculadora

---
+++ Calculadora 2/Assets/Calculadora.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Calculadora : MonoBehaviour
+{
+    public TMP_InputField primerNumero;
+    public TMP_InputField segundoNumero;
+    public TMP_Text resultado;
+
+
+    //Variables
+    //Metodos
+}
